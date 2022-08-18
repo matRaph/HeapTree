a428cc7d53ef298a667867bd68e626ff7cf92e4c
Raphael Matias <raphaelmatias135790@gmail.com>
2022-08-18 14:16:41 -0300

    Adicionar arquivos de projeto.

---
+++ HeapTree.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapTree
+{
+    internal class HeapTree
+    {
+        internal class Node
+        {
+            public int Key { get; set; }
+            public object Element { get; set; }
+            public Node? Parent { get; set; }
+            public Node? LeftChild { get; set; }
+            public Node? RightChild { get; set; }
+            public Node(int key, object element, Node parent)
+            {
+                Key = key;
+                Element = element;
+                Parent = parent;
+            }
+        }
+        
+        public class HeapTree
+        {
+            private Node Root { get; set; }
+            private Node LastNode { get; set; }
+            private Node InsertionNode { get; set; } 
+            public int Size { get; set; }
+            public HeapTree(int rootKey, object rootElement)
+            {
+                Root = new Node(rootKey, rootElement, null);
+                InsertionNode = Root.LeftChild;
+                InsertionNode.Parent = Root;
+                LastNode = Root;
+                Size = 1;
+            }
+            public bool IsEmpty()
+            {
+                return this.Size == 0;
+            }
+            public void Insert(object element, int key)
+            {
+                this.InsertionNode.Element = element;
+                this.InsertionNode.Key = key;
+                this.Size++;
+                UpHeap();
+                NewInsertionNode();
+            }
+            private void UpHeap()
+            {
+
+            }
+            private void NewInsertionNode()
+            {
+
+            }
+        }
+    }
+}
